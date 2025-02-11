550d892e3525424e36486e79595a2be6dba44c54
juanmartinezdb <juanmmcontacto@gmail.com>
2025-02-11 14:22:42 +0100

    depurados warnings de terminal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {RouterModule} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HousingLocationComponent, RouterModule, RouterLink],
+  imports: [RouterOutlet, RouterModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
